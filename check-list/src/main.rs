fe46d988ae4e66e4a1bc004f9ad9164d339ab1f8
Vinicius-notebook <vinicius.oliveirafonseca@gmail.com>
2019-02-01 02:26:41 -0300

    Second Commit

---
+++ check-list/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+
+    let mut list = Vec::new():
+    list.push("Test 1");
+    list.push("Test 2");
+
+    //It is a test
+
+    while(true){
+        let mut input = io::stdin().read_line()
+            .expect("Problem from input");
+    }
+}
